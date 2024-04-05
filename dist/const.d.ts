d14b26ed0dc82485847a4d74186ce7e95c5875ad
Erkki Lukk <erkki.lukk@getivy.de>
2024-04-05 14:11:06 +0100

    feat(swan): add dist

---
+++ dist/const.d.ts
@@ -0,0 +1,6 @@
+export declare const AllTypesProps: Record<string, any>;
+export declare const ReturnTypes: Record<string, any>;
+export declare const Ops: {
+    query: "Query";
+    mutation: "Mutation";
+};
